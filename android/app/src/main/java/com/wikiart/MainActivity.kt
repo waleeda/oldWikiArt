d49bf51d155cd6eddd42b3097bcfd8f0c4913ced
Waleed Azhar <waleeda@icloud.com>
2025-06-11 02:13:30 -0400

    Add painting category filtering

--- android/app/src/main/java/com/wikiart/MainActivity.kt
+++ android/app/src/main/java/com/wikiart/MainActivity.kt
@@ -2,11 +2,16 @@ package com.wikiart
 
 import android.os.Bundle
 import android.content.Intent
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.paging.cachedIn
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +22,9 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    private val repository = PaintingRepository()
+    private var pagingJob: Job? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,9 +33,26 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = adapter
 
-        val repository = PaintingRepository()
-        lifecycleScope.launch {
-            repository.pagingFlow()
+        val spinner: Spinner = findViewById(R.id.categorySpinner)
+        val categories = PaintingCategory.values()
+        spinner.adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, categories)
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                loadCategory(categories[position])
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
+
+        // Load default category
+        loadCategory(PaintingCategory.FEATURED)
+    }
+
+    private fun loadCategory(category: PaintingCategory) {
+        pagingJob?.cancel()
+        pagingJob = lifecycleScope.launch {
+            repository.pagingFlow(category)
                 .cachedIn(lifecycleScope)
                 .collect { pagingData ->
                     adapter.submitData(pagingData)
--- android/app/src/main/java/com/wikiart/WikiArtService.kt
+++ android/app/src/main/java/com/wikiart/WikiArtService.kt
@@ -11,8 +11,22 @@ class WikiArtService(
     private val client = OkHttpClient()
     private val gson = Gson()
 
-    fun fetchFeaturedPaintings(page: Int = 1): PaintingList? {
-        val url = serverConfig.apiBaseUrl.toString() + "/" + language + "?json=2&param=featured&page=" + page
+
+    fun fetchFeaturedPaintings(page: Int = 1): PaintingList? =
+        fetchPaintings(PaintingCategory.FEATURED, page)
+
+    fun fetchPaintings(category: PaintingCategory, page: Int = 1): PaintingList? {
+        val url = when (category) {
+            PaintingCategory.FEATURED ->
+                "${serverConfig.apiBaseUrl}/$language?json=2&param=featured&page=$page"
+            PaintingCategory.POPULAR ->
+                "${serverConfig.apiBaseUrl}/$language/popular-paintings/alltime?page=$page&json=2"
+            PaintingCategory.HIGH_RES ->
+                "${serverConfig.apiBaseUrl}/$language?json=2&param=high_resolution&page=$page"
+            else ->
+                "${serverConfig.apiBaseUrl}/$language?json=2&param=featured&page=$page"
+        }
+
         val request = Request.Builder().url(url).build()
         client.newCall(request).execute().use { response ->
             if (!response.isSuccessful) return null
