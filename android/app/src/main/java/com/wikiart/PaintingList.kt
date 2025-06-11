1246303fece92cf98ca7da49def7dfee3d75fd95
Waleed Azhar <waleeda@icloud.com>
2025-06-11 01:10:59 -0400

    Add basic API models and service for Android

---
+++ android/app/src/main/java/com/wikiart/PaintingList.kt
@@ -0,0 +1,12 @@
+package com.wikiart
+
+import com.google.gson.annotations.SerializedName
+
+data class PaintingList(
+    @SerializedName("Paintings") val paintings: List<Painting>,
+    @SerializedName("AllPaintingsCount") val allPaintingsCount: Int,
+    @SerializedName("PageSize") val pageSize: Int
+) {
+    val pageCount: Int
+        get() = (allPaintingsCount / pageSize) + 1
+}
