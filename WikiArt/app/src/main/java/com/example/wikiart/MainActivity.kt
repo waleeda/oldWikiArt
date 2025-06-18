9ca5d4a21deeedce5cd39d4909e8c89a2b4c3bf7
Waleed Azhar <waleeda@icloud.com>
2025-06-18 17:32:19 -0400

    Add new navigation sections

--- WikiArt/app/src/main/java/com/example/wikiart/MainActivity.kt
+++ WikiArt/app/src/main/java/com/example/wikiart/MainActivity.kt
@@ -26,7 +26,10 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_paintings,
+                R.id.navigation_artists,
+                R.id.navigation_search,
+                R.id.navigation_support
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
