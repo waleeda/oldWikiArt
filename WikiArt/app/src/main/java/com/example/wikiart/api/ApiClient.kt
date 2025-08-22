7279db0768864767b1caee34ba4a54da08cf8e9d
Waleed Azhar <waleeda@icloud.com>
2025-08-22 14:57:50 -0400

    Add Moshi Kotlin support and configure converter

--- WikiArt/app/src/main/java/com/example/wikiart/api/ApiClient.kt
+++ WikiArt/app/src/main/java/com/example/wikiart/api/ApiClient.kt
@@ -1,5 +1,7 @@
 package com.example.wikiart.api
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
@@ -8,10 +10,14 @@ object ApiClient {
 
     var serviceOverride: WikiArtService? = null
 
+    private val moshi: Moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
     private val defaultService: WikiArtService by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(MoshiConverterFactory.create())
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
             .create(WikiArtService::class.java)
     }
